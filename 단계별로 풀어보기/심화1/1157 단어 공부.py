20670124efecddcd2d3eaf45a34bfa41992e161a
zzoo0123 <0123suh@gmail.com>
2024-07-25 11:41:50 +0900

    단어 공부 수정

--- 단계별로 풀어보기/심화1/1157 단어 공부.py
+++ 단계별로 풀어보기/심화1/1157 단어 공부.py
@@ -1,10 +1,17 @@
+# 시간 복잡도: O(n^2)
+# word = input().upper()
+# word_list = list(set(word))
+# cnt = []
+#
+# for i in word_list:
+#     cnt.append(word.count(i))
+#
+# print("?" if cnt.count(max(cnt)) > 1 else word_list[cnt.index(max(cnt))])
+
+# 시간 복잡도: O(n)
+from collections import Counter
+
 word = input().upper()
-word_list = list(set(word))
-cnt = []
+arr = Counter(word).most_common()
 
-for i in word_list:
-    count = word.count(i)
-    cnt.append(count)
-
-print("?") if cnt.count(max(cnt)) > 1 else print(word_list[cnt.index(max(cnt))])
-
+print("?" if len(arr) > 1 and arr[0][1] == arr[1][1] else arr[0][0])
